68f44df72600a6bf64060244c20e3ce7afdcd107
hulc <3314986382@qq.com>
2025-06-29 00:30:19 +0800

    build files

--- build.sh
+++ build.sh
@@ -1,4 +1,12 @@
+#!/bin/zsh
 rm -rf build
 mkdir build && cd build
-cmake .. -DCUDAToolkit_ROOT=/usr/local/cuda-12.8 -DCMAKE_CUDA_COMPILER=/usr/local/cuda-12.8/bin/nvcc -DPython3_ROOT_DIR=/usr/lib/python3.10
-make
+cmake .. \
+    -DCUDAToolkit_ROOT=/opt/cuda \
+    -DCMAKE_CUDA_COMPILER=/opt/cuda/bin/nvcc \
+    -DPython3_ROOT_DIR=/usr/lib/python3.13 \
+&& make
+cd ..
+rm ./src/mlp_ac_cuda.cpython-313-x86_64-linux-gnu.so
+cp ./build/mlp_ac_cuda.cpython-313-x86_64-linux-gnu.so ./src
+
